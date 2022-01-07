bb68dbd0af7bbb01a6227741f5353f865a2f876f
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-07 11:29:50 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: ".",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "eslint-config-prettier",
+  ],
+  plugins: ["prettier", "@typescript-eslint", "import"],
+  env: {
+    es6: true,
+    node: true,
+  },
+};
